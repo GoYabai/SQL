e015f99561497876fef1e51222b950cfa25ea0c2
GoYabai <leduchuyqazzaq@gmail.com>
2026-07-27 21:08:32 +0700

    SQL CASE

---
+++ tutorial/SQL_CASE/Superheroes_Likes.sql
@@ -0,0 +1,12 @@
+SELECT
+  actor,
+  character,
+  platform,
+  avg_likes,
+  CASE
+    WHEN avg_likes >= 15000 THEN 'Super Likes'
+    WHEN avg_likes >= 5000 THEN 'Good Likes'
+    ELSE 'Low Likes'
+  END AS likes_category
+FROM marvel_avengers
+ORDER BY avg_likes DESC;
